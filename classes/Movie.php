e5d181501220f6393c3fc937104bb92a86c0ee82
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-10-09 16:32:16 +0200

    moved class istances in index.php from movie.php

--- classes/Movie.php
+++ classes/Movie.php
@@ -24,8 +24,4 @@ class Movie {
   }
 }
 
-$green_book = new Movie('Green book', 'biografico', 130, '', '', []);
-$lady_bird = new Movie('Lady bird', 'commedia', 93, '', '', []);
-var_dump($green_book);
-var_dump($lady_bird);
-; ?>
+?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php 
-require_once __DIR__ . '/classes/Movie.php'
-; ?>
+require_once __DIR__ . '/classes/Movie.php';
+
+$green_book = new Movie('Green book', 'biografico', 130, '', '', []);
+$lady_bird = new Movie('Lady bird', 'commedia', 93, '', '', []);
+var_dump($green_book);
+var_dump($lady_bird);
+?>
